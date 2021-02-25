777c0d2da5d709dbdfbe3d2402c68316ccb9ac68
liujiang <liujiang1214@ichinae.com>
2021-02-25 11:33:05 +0800

    feat:set "padding\margin\linearGradient"

--- containerwidget/lib/main.dart
+++ containerwidget/lib/main.dart
@@ -4,7 +4,7 @@
  * @Author: jiang.liu
  * @Date: 2021-02-25 10:53:32
  * @LastEditors: jiang.liu
- * @LastEditTime: 2021-02-25 11:05:03
+ * @LastEditTime: 2021-02-25 11:31:11
  */
 import 'package:flutter/material.dart';
 
@@ -19,20 +19,27 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('hahahahahahhahahahhahahahahhahaha'),
+          title: Text('Hello,shuai'),
         ),
         body: Center(
           child: Container(
             child: new Text(
-              'Hello,shuai',
+              'Hello,shuai,hahahahahahhahahahhahahahahhahaha',
               style: TextStyle(
                 fontSize: 40.0,
               ),
             ),
-            alignment: Alignment.center,
+            alignment: Alignment.topLeft,
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+            // color: Colors.lightBlue,
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.pink],
+              ),
+            ),
           ),
         ),
       ),
